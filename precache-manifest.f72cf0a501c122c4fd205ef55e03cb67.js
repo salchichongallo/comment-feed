8cccce5c81316d3ea777dda70fb2d270525c0112
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-17 21:51:03 +0000

    Deploy salchichongallo/test-comment-feed to github.com/salchichongallo/test-comment-feed.git:gh-pages

---
+++ precache-manifest.f72cf0a501c122c4fd205ef55e03cb67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78e114cb2b860217ca4e15cd995b6ee",
+    "url": "/comment-feed/index.html"
+  },
+  {
+    "revision": "b15683e64621588b37cc",
+    "url": "/comment-feed/static/css/main.694a01a7.chunk.css"
+  },
+  {
+    "revision": "4e9b1380d5b649513471",
+    "url": "/comment-feed/static/js/2.47739b5d.chunk.js"
+  },
+  {
+    "revision": "b15683e64621588b37cc",
+    "url": "/comment-feed/static/js/main.0ab7c4f9.chunk.js"
+  },
+  {
+    "revision": "f54c99f5eec159147084",
+    "url": "/comment-feed/static/js/runtime-main.9ab1f614.js"
+  },
+  {
+    "revision": "fb7c94cfb01698aa1c45138bd1e2b6b7",
+    "url": "/comment-feed/static/media/star.fb7c94cf.svg"
+  }
+]);
--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Button.css';
-
-function Button(props) {
-  return <button type="button" {...props} className="Button" />;
-}
-
-export default Button;
